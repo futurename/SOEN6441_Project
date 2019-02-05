4e5563a4de5db829543585462d27c27b9040e2e2
athrunecho <xu_tian_cheng@outlook.com>
2019-02-05 00:03:16 -0500

    Add implementation of MEContinent and MECountry.

--- RiskGameProject/mapEditor/classes/MEContinent.java
+++ RiskGameProject/mapEditor/classes/MEContinent.java
@@ -1,5 +1,36 @@
-package mapEditor.classes;
+package classes;
+
+import java.util.LinkedList;
 
 public class MEContinent{
+    private String continentName;
+    private int bonus;
+    private LinkedList<String> countryList = new LinkedList<String>();
+
+    public void setContinentName(String newContinentName){
+
+        this.continentName = newContinentName;
+    }
+
+    public void setBonus(int bonus){
+
+        this.bonus = bonus;
+    }
+
+    public boolean addCountry(String countryName){
+        if(!countryList.contains(countryName)){
+            return countryList.add(countryName);
+        }
+        return false;
+    }
+
+    public String getContinentName(){
+        return continentName;
+    }
+
+    public String getcountryList(){
+        return countryList.toString();
+    }
+
 
 }
--- RiskGameProject/mapEditor/classes/MECountry.java
+++ RiskGameProject/mapEditor/classes/MECountry.java
@@ -1,5 +1,28 @@
-package mapEditor.classes;
+package classes;
+
+import java.util.LinkedList;
 
 public class MECountry{
+    private String countryName;
+    private LinkedList<String> adjacentCountry = new LinkedList<String>();
 
+    public void setCountryName(String newCountryName){
+        this.countryName = newCountryName;
+    }
+
+    public boolean setAdjacentCountry(String AdjacentCountryName){
+
+        if(!adjacentCountry.contains(AdjacentCountryName)){
+            return adjacentCountry.add(AdjacentCountryName);
+        }
+        return false;
+    }
+
+    public String getCountryName(){
+        return countryName;
+    }
+
+    public String getAdjacentCountry(){
+        return adjacentCountry.toString();
+    }
 }
