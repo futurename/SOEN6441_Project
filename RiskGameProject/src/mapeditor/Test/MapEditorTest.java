827c3763a2a1189b672d10c13e3e261a57b92a4e
dianlutuabc <jeffrey95@163.com>
2019-03-02 17:45:03 -0500

    Map Editor junit test frame

--- RiskGameProject/src/mapeditor/Test/MapEditorTest.java
+++ RiskGameProject/src/mapeditor/Test/MapEditorTest.java
@@ -1,4 +1,32 @@
 package mapeditor.Test;
 
+
+import mapeditor.MEMain;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MapEditorTest {
+
+    MEMain testMEMain;
+    @Before public void before(){
+        testMEMain = new MEMain();
+        testMEMain.createContinent("testfirstcontinent",1);
+        testMEMain.createsSoloCountry("testfirstcountry");
+    }
+
+    @Test public void testMEMainCreateContinent(){
+
+    }
+
+    @Test public void testMEMainCreateCountry(){
+
+    }
+
+    @Test public void testMEMainDeleteCountry(){
+
+    }
+
+    @Test public void testMEMainDeleteContinent (){
+
+    }
 }
