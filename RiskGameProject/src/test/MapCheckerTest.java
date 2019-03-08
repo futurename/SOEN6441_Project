0c0210175ce0ad742270eaca7d7e8832a750b4c8
ZhanfanZhou <zzffanky@gmail.com>
2019-03-07 19:04:14 -0500

    update MapCheckerTest.java

--- RiskGameProject/src/test/MapCheckerTest.java
+++ RiskGameProject/src/test/MapCheckerTest.java
@@ -6,32 +6,45 @@ import riskgame.model.Utils.MapChecker;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 /**
  * MapChecker tester.
  */
 
 public class MapCheckerTest {
-    private String path, error_path, error_map;
-    private String newMapPath = "maps/USA.map";
+    private ArrayList loadableMaps;
+    private ArrayList errorMaps;
+    private String error_path;
 
     @Before
     public void setUp() throws Exception {
-        path = "./maps/World.map";
+        loadableMaps = new ArrayList<String>();
+        errorMaps = new ArrayList<String>();
+        loadableMaps.add("./maps/World.map");
+        loadableMaps.add("./maps/Alberta.map");
+        loadableMaps.add("./maps/test_map.map");
+        loadableMaps.add("./maps/USA.map");
+        errorMaps.add("./maps/ErrorMap_ContinentsAmount_United States.map");
+        loadableMaps.add("./maps/World-error.map");
+
         error_path = "./maps/World";
-        error_map = "./maps/TestWorld.map";
     }
 
     @Test
     public void checkMapValidity() throws IOException {
+        for (int i=0;i<loadableMaps.size();i++){
+            assertEquals(9527, MapChecker.checkMapValidity((String) this.loadableMaps.get(i)));
+        }
+    }
 
-        System.out.println((new File(newMapPath).getAbsolutePath()));
-
-        assertEquals(9527, MapChecker.checkMapValidity(this.newMapPath));
-
+    @Test
+    public void isMapValid() throws IOException {
+        for (int i=0;i<errorMaps.size();i++){
+            assertTrue(MapChecker.isMapValid((String) errorMaps.get(i)));
+        }
     }
 
     @Test
