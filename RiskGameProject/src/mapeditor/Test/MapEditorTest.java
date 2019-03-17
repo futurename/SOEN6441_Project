6aa23d770764c63d1f2b9ac6efe250c6fa866646
athrunecho <xu_tian_cheng@outlook.com>
2019-03-17 03:11:10 -0400

    refactor test cases

--- RiskGameProject/src/mapeditor/Test/MapEditorTest.java
+++ RiskGameProject/src/mapeditor/Test/MapEditorTest.java
@@ -2,12 +2,17 @@ package mapeditor.Test;
 
 
 import mapeditor.MEMain;
+import mapeditor.model.MECheckMapCorrectness;
 import mapeditor.model.MEContinent;
 import mapeditor.model.MECountry;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,27 +20,22 @@ import static org.junit.Assert.assertEquals;
 public class MapEditorTest {
 
     public static MEMain testMEMain;
-    public String firstmap = "maps/ErrorMap/World-firsterror.map";
-    public String secondmap = "maps/ErrorMap/World-seconderror.map";
     public String thirdmap = "maps/ErrorMap/World-thirderror.map";
 
-    @BeforeClass public static void before(){
-        testMEMain = new MEMain();
-
-
-        MEMain.createContinent("testFirstContinent",1);
-        MEMain.createsSoloCountry("testFirstCountry");
-        MEMain.arrMEContinent.get(0).addCountry("testFirstCountry");
-        MEMain.createsSoloCountry("testSecondCountry");
-
+    @BeforeClass
+    public static void before(){
+        testMEMain.createContinent("testFirstContinent",1);
+        testMEMain.createsSoloCountry("testFirstCountry");
+        testMEMain.arrMEContinent.get(0).addCountry("testFirstCountry");
+        testMEMain.createsSoloCountry("testSecondCountry");
         System.out.println("Test map editor");
     }
 
     /**
      * Check add continent function
      */
-    @Test public void testMEMainCreateContinent(){
-        MEContinent newContinent =  MEMain.arrMEContinent.get(0);
+    @Test public void testA(){
+        MEContinent newContinent =  testMEMain.arrMEContinent.get(0);
         assertEquals("testFirstContinent",newContinent.getContinentName());
         assertEquals(1,newContinent.getBonus());
     }
@@ -43,50 +43,104 @@ public class MapEditorTest {
     /**
      * Check add country function
      */
-    @Test public void testMEMainCreateCountry(){
-        MECountry newCountry =  MEMain.arrMECountry.get(0);
+    @Test public void testB(){
+        MECountry newCountry =  testMEMain.arrMECountry.get(0);
         assertEquals("testFirstCountry",newCountry.getCountryName());
     }
 
     /**
      * Check delete continent function
      */
-    @Test public void testMEMainDeleteContinent () {
-        MEMain.deleteContinent("testFirstContinent");
-        assertEquals(0, MEMain.arrMEContinent.size());
-        assertEquals(1, MEMain.arrMECountry.size());
+    @Test public void testC() {
+        testMEMain.deleteContinent("testFirstContinent");
+        assertEquals(0, testMEMain.arrMEContinent.size());
+        assertEquals(1, testMEMain.arrMECountry.size());
     }
 
     /**
      * Check delete country function
      */
-    @Test public void testMEMainDeleteCountry(){
-        MEMain.deleteCountry("testSecondCountry");
-        assertEquals(0, MEMain.arrMECountry.size());
+    @Test public void testD() {
+        testMEMain.deleteCountry("testSecondCountry");
+        assertEquals(0, testMEMain.arrMECountry.size());
     }
 
     /**
      * Test for checking whether all countries in map are connected
-     *//*
-    @Test public void testCheckConnectGraph(){
-        MapObject mapObject = new MapObject();
-        assertFalse(mapObject.checkCorrectness(firstmap));
+     */
+    @Test public void testE() throws Exception{
+        testMEMain.arrMEContinent.clear();
+        testMEMain.arrMECountry.clear();
+        readMap("maps/ErrorMap/World-firsterror.map");
+        MECheckMapCorrectness mapCheck = new  MECheckMapCorrectness();
+        assertEquals( "Unconnected Graph",mapCheck.isCorrect(testMEMain.arrMECountry, testMEMain.arrMEContinent));
     }
 
-    *//**
+    /**
      * Test for checking whether all countries in one continent are placed together
-     *//*
-    @Test public void testCheckContinentCountry(){
-        MapObject mapObject = new MapObject();
-        assertFalse(mapObject.checkCorrectness(secondmap));
+     */
+    @Test public void testF() throws Exception{
+        testMEMain.arrMEContinent.clear();
+        testMEMain.arrMECountry.clear();
+        readMap("maps/ErrorMap/World-sixtherror.map");
+        MECheckMapCorrectness mapCheck = new  MECheckMapCorrectness();
+        assertEquals( "Country doesn't connect with any country of its continent",mapCheck.isCorrect(testMEMain.arrMECountry, testMEMain.arrMEContinent));
     }
 
-    *//**
+    /**
      * Test for checking whether a country only belongs to one continent
-     *//*
-    @Test public void testCheckContinentBelonging(){
-        MapObject mapObject = new MapObject();
-        assertFalse(mapObject.checkCorrectness(thirdmap));
-    }*/
+     */
+    @Test public void testG() throws Exception{
+        testMEMain.arrMEContinent.clear();
+        testMEMain.arrMECountry.clear();
+        readMap("maps/ErrorMap/World-thirderror.map");
+        MECheckMapCorrectness mapCheck = new  MECheckMapCorrectness();
+        assertEquals( "Country belongs to mutiple continent",mapCheck.isCorrect(testMEMain.arrMECountry, testMEMain.arrMEContinent));
+    }
 
+    private void readMap(String path)throws Exception{
+        ArrayList<String> fileRead = new ArrayList<String>();
+        try {
+            File file=new File(path);
+            if(file.isFile()&&file.exists()){
+                InputStreamReader read = new InputStreamReader(new FileInputStream(file));
+                BufferedReader bufferedReader=new BufferedReader(read);
+                String lineTxt=null;
+                while((lineTxt=bufferedReader.readLine())!=null){
+                    fileRead.add(lineTxt);
+                }
+                read.close();
+            }else{
+                System.out.println("cannot find file");
+            }
+        } catch (Exception e){
+            System.out.println("wrong");
+            e.printStackTrace();
+        }
+        for(int i = 0;i<fileRead.size();i++){
+            if(fileRead.get(i).equals("[Continents]")){
+                for(int j=i+1;j<fileRead.size();j++){
+                    if(!fileRead.get(j).equals("")){
+                        testMEMain.createContinent(fileRead.get(j).split("=")[0],Integer.parseInt(fileRead.get(j).split("=")[1]));
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+            else if(fileRead.get(i).equals("[Territories]")){
+                for(int k=i+1;k<fileRead.size();k++){
+                    if(!fileRead.get(k).equals("")){
+                        String[] countrydata = fileRead.get(k).split(",");
+                        testMEMain.createCountry(countrydata[0],countrydata);
+                        for(int z=0; z<testMEMain.arrMEContinent.size();z++) {
+                            if(testMEMain.arrMEContinent.get(z).getContinentName().equals(countrydata[3]))
+                                testMEMain.arrMEContinent.get(z).addCountry(countrydata[0]);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 }
