89dd90f8fe684d6f4703b10ccf7dcedf762c7e19
ZhanfanZhou <zzffanky@gmail.com>
2019-03-28 20:13:06 -0400

    strategy

---
+++ RiskGameProject/src/riskgame/model/BasicClass/StrategyPattern/StrategyRandom.java
@@ -0,0 +1,18 @@
+package riskgame.model.BasicClass.StrategyPattern;
+
+public class StrategyRandom implements Strategy {
+    @Override
+    public void doReinforcement() {
+
+    }
+
+    @Override
+    public void doAttack() {
+
+    }
+
+    @Override
+    public void doFortification() {
+
+    }
+}
